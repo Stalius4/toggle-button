54cf95ae042c231fa224d0e4adfa954cfb49ce71
Deivydas Rimgaila <99248199+Stalius4@users.noreply.github.com>
2024-03-09 20:01:01 +0000

    toggle logit moved to a+b button from forever tab

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 let ab_was_released = false
-input.onButtonPressed(Button.AB, function () {
-    if (input.buttonIsPressed(Button.AB) && ab_was_released) {
-        ab_was_released = false
-    }
-    ab_was_released = !(ab_was_released)
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
 })
-basic.forever(function () {
+input.onButtonPressed(Button.AB, function () {
     if (ab_was_released == false) {
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
@@ -14,4 +12,15 @@ basic.forever(function () {
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 1)
     }
+    if (input.buttonIsPressed(Button.AB) && ab_was_released) {
+        ab_was_released = false
+    }
+    ab_was_released = !(ab_was_released)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+basic.forever(function () {
+	
 })
